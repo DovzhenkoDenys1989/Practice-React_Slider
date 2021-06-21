7a2e21507eb2b6e79ee0b6a182bb53e840b7737b
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-06-21 16:25:25 +0300

    Practice

---
+++ src/components/SliderData/index.jsx
@@ -0,0 +1,17 @@
+export const SliderData = [
+  {
+    image: 'https://online-vehicle.brandcrock.com/wp-content/uploads/sites/4/2013/05/slider_1_2.jpg'
+  },
+  {
+    image: 'https://bluepointperformance.com/wp-content/uploads/2018/02/slider_1_61-11.png'
+  },
+  {
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeBHsTUq7I-o9Yb5XgBnhB9h2Bepb0hSfflN-42Lw4S4fdFbzBKJoAQb6p-v5qSsbsbPQ&usqp=CAU'
+  },
+  {
+    image: 'https://lh3.googleusercontent.com/proxy/6rwH5daYvUuCX1bbxFkAbNadXggk51miaGyw0IGwWEluFaBfDMIgJ3wJwSTQ6BEygj3FjSaVx-5jaF-P2XoSv4czCT-FauJuhxNYUim6m4ZCtjzono0QHlS-4jedAFKY'
+  },
+  {
+    image: 'https://static7.depositphotos.com/1062035/744/i/600/depositphotos_7447240-stock-photo-rear-side-view-of-car.jpg'
+  },
+]
